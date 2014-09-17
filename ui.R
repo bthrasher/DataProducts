348d9070464ed2e55504f42aae87b8cd02de785a
bthrasher <bthrasher@gmail.com>
2014-09-17 15:31:03 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,10 +2,11 @@ library(shiny)
 
 shinyUI(fluidPage(
      titlePanel("How tall will my son be?"),
-     textInput("fht", label = h3("Father's height in inches"), 
+     textInput("fht", label = h3("Enther the father's height in inches:"), 
                value = "72"),
      hr(),
      h4("Your son has a 95% chance of being in the following range:"),
      h4(textOutput("value"))
      
 ))
+
